13f5b0b044ce7fc8d7e31084f90522165d2a7c02
imballama <Inkvizzzz@gmail.com>
2023-06-14 13:46:53 +0300

    implement updatetion route

--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -14,3 +14,30 @@ def create_new_job(job: JobCreate, db: Session, owner_id: int):
 def retreive_job(id: int, db: Session):
     item = db.query(Job).filter(Job.id == id).first()  # select * from job where id = 1;
     return item
+
+
+def list_jobs(db: Session):
+    jobs = db.query(Job).filter(Job.is_active == True).all()
+    return jobs
+
+
+def update_job_by_id(id: int, job: JobCreate, db: Session, owner_id):
+    existing_job = db.query(Job).filter(Job.id == id)
+    # print(type(existing_job))
+    # SELECT job.id AS job_id, job.title AS job_title, job.company AS job_company, job.company_url AS job_company_url,
+    # job.location AS job_location, job.description AS job_description, job.date_posted AS job_date_posted,
+    # job.is_active AS job_is_active, job.owner_id AS job_owner_id
+    # FROM job
+    # WHERE job.id = %(id_1)s
+    if not existing_job.first():
+        return 0
+    # print(job.__dict__)
+    # {'title': 'string', 'company': 'string', 'company_url': 'string', 'location': 'string',
+    # 'description': 'string', 'date_posted': datetime.date(2023, 6, 14)}
+    job.__dict__.update(
+        owner_id=owner_id
+    )  # update dictionary wit new key value of owner_id
+    print(job)
+    existing_job.update(job.__dict__)
+    db.commit()
+    return 1
